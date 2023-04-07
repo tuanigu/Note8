cbced43173072341a5246025f2c6664bdc1c4ab5
dang thanh tuan <90979922+tuanigu@users.noreply.github.com>
2023-04-08 01:07:37 +0700

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w dero1qyhyqvj4jsgkeej9agw5wax3sc6p7jmphcztdvsvej5wdh599ntfxqqmxtvpd-r community-pools.mysrv.cloud:10300 -r1 dero.friendspool.club:10300 -r2 nodent2.cpumining.cloud:10100-p rpc;
-    sleep 5;
-done
